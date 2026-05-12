54d577c2ddcf9e7b7e0db6cd3568e45de44ac169
steffaniruth3407 <steffaniruth@gmail.com>
2026-05-12 15:23:38 -0400

    Create main.scala

---
+++ main.scala
@@ -0,0 +1,8 @@
+// MIT License
+// Copyright (c) 2026 brandongersbachhoquxo001915
+
+object Main {
+    def main(args: Array[String]): Unit = {
+        println("Windows Activation Education Pack placeholder")
+    }
+}
